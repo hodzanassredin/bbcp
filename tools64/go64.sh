cf1a13821ce44adeeb43f01de3b609eb93021734
Anton Shelin <shelinanton@gmail.com>
2026-07-24 02:51:16 +0300

    LinDl 64-бит (PtrVoid=LONGINT, [ccall16]→[ccall] SysV) + go64.sh stash bbcp64use/Dev

--- tools64/go64.sh
+++ tools64/go64.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Compile modules (args or /tmp/compile1.txt) as amd64 into bbcp64use.
 # Usage: go64.sh Kernel64 Files64 ...
-cd "$HOME/sources/bbcp64use"
+# bbcp64use/Dev на время прячем: 64-битные DevCP*.ocf ломают dev0
+# ("corrupted code file"). Для модулей Dev* использовать build-dev64.sh!
+set -e
+USE="$HOME/sources/bbcp64use"
+STASH="$USE/.dev-stash-go64"
+[ -d "$USE/Dev" ] && mv "$USE/Dev" "$STASH"
+trap '[ -d "$STASH" ] && mv "$STASH" "$USE/Dev"' EXIT
+cd "$USE"
 [ $# -gt 0 ] && printf '%s\n' "$@" > /tmp/compile1.txt
 echo 'DevOnce.Go64' | "$HOME/sources/bbcp/run-dev0"
